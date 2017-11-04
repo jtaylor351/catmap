ed403928ccf5a375a1f3e8cf6fddaea59fbbd45e
jtaylor351 <jtaylor351@gatech.edu>
2017-11-04 06:31:38 -0400

    fixing bugs

---
+++ README.md
@@ -0,0 +1,2 @@
+# catmap
+App for GT Healthcare Hackathon that helps women report catcallers and find safe paths to walk home
---
+++ app/src/main/java/com/example/jordan/catmap/model/User.java
@@ -0,0 +1,9 @@
+package com.example.jordan.catmap.model;
+
+/**
+ * Created by Jordan on 11/4/2017.
+ */
+
+public class User {
+    private String name;
+}
